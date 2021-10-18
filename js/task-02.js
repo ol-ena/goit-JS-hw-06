9754c0f2d11d1282987c1c2045fe07ad7bf9543a
ol-ena <ena24404t@gmail.com>
2021-10-18 10:08:53 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createList = [];
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement(`li`);
+  li.textContent = ingredient;
+  li.classList.add('item'); 
+  createList.push(li);
+});
+
+document.querySelector('ul').append(...createList);
+
+//const createList = document.createElement(`li`);
+//ingredients.forEach(element => console.log(element));
+//createList.textContent = ``;
+//createList.classList.add(item);
